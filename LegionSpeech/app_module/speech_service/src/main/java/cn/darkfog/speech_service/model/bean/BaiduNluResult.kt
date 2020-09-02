b2d7a70e182b43077ead6c816423dd86bf3e3b04
saint2078 <lei.cl.chen@bmw.com>
2020-09-02 15:21:50 +0800

    ts
    Signed-off-by: saint2078 <lei.cl.chen@bmw.com>

---
+++ LegionSpeech/app_module/speech_service/src/main/java/cn/darkfog/speech_service/model/bean/BaiduNluResult.kt
@@ -0,0 +1,32 @@
+package cn.darkfog.speech_service.model.bean
+
+import cn.darkfog.speech_protocol.speech.bean.NluResult
+
+data class BaiduNluResponse(
+    val appid: Int,
+    val encoding: String,
+    val err_no: Int,
+    val parsed_text: String,
+    val raw_text: String,
+    val results: List<BaiduNluResult>
+) {
+    fun toNluResult(): NluResult {
+
+    }
+}
+
+data class BaiduNluResult(
+    val domain: String,
+    val intent: String,
+    val score: Double,
+    val slots: Slots
+)
+
+data class Slots(
+    val user_singer_name: List<UserSingerName>
+)
+
+data class UserSingerName(
+    val norm: String,
+    val word: String
+)
