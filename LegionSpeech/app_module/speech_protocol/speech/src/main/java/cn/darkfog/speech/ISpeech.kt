0472c8e7f477180001c663c41633df2ef2dba4cc
saint2078 <lei.cl.chen@bmw.com>
2020-12-01 16:46:02 +0800

    delete useless code
    Signed-off-by: saint2078 <lei.cl.chen@bmw.com>

--- LegionSpeech/app_module/speech_protocol/speech/src/main/java/cn/darkfog/speech/ISpeech.kt
+++ LegionSpeech/app_module/speech_protocol/speech/src/main/java/cn/darkfog/speech/ISpeech.kt
@@ -1,13 +1,9 @@
 package cn.darkfog.speech
 
 import android.os.Bundle
-import io.reactivex.Observer
 
 interface ISpeech {
 
     fun init(extra : Bundle? =null)
-
-    fun startWakeUp() : Observer<>
-
 
 }
